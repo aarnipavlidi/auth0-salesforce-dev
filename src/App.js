3838517368479dae0afbf923408c8e06239293e1
aarnipavlidi <me@aarnipavlidi.fi>
2022-03-18 17:40:06 +0200

    Logging/Logging out works. Takes data from different db.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+const App = () => {
+  
+  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  if (user && isAuthenticated) {
+    return (
+    <div className="container d-flex">
+      <div>
+        <h1>Olet kirjautunut sisään.</h1>
+        <button onClick={logout}>Kirjaudu ulos</button>
+      </div>
+    </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex">
+      <div>
+        <h1>Kirjaudu Sisään</h1>
+        <button onClick={loginWithRedirect}>Login</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
